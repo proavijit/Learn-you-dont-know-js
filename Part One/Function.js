4919c06870292b54c5f2c59f2f15f00739b63bda
proavijit <avijitghosh2268@gmail.com>
2025-01-21 11:02:19 +0600

    first commit

---
+++ Part One/Function.js
@@ -0,0 +1,34 @@
+
+// function printAmount(amt){
+//   console.log(amt.toFixed(2));
+// }
+
+// function formatAmount(){
+//   return "$" + amount.toFixed(2);
+// }
+
+// var amount = 99.99;
+// printAmount(amount * 2);
+
+// amount = formatAmount();
+
+// console.log(amount)
+
+
+
+// const TAX_RATE = 0.08;
+
+// function calculateFinalPurchaseAmount(amt){
+//   amt = amt + (amt * TAX_RATE);
+//   return amt
+// }
+
+// var amount = 99.88;
+
+// amount = calculateFinalPurchaseAmount(amount);
+
+// console.log(amount.toFixed(2))
+
+
+// উপসংহার
+// ফাংশন প্রোগ্রামের গুরুত্বপূর্ণ একটি অংশ, কারণ এটি কোড পুনরায় ব্যবহারযোগ্য এবং সংগঠিত করে। এর মাধ্যমে কোডের জটিলতা কমে এবং পড়তে সহজ হয়।
