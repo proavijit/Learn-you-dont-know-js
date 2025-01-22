71e237c1b3ef2cfb485225edfc9b479581015ff1
proavijit <avijitghosh2268@gmail.com>
2025-01-22 23:09:09 +0600

    gurbase collection

---
+++ JS-Scope-Closures/gurbase collection.js
@@ -0,0 +1,25 @@
+function process(data){
+  // something inportant 
+}
+
+var someReallyBigDtata = (...);
+
+process(someReallyBigData);
+
+var btn = document.getElementById("my_button");
+btn.addEventListener("click", function click(evt){
+  console.log("button clicked");
+})
+
+
+function process(data){
+  // some work.......
+}{
+  let someReallyBigData = {...};
+  process(someReallyBigData);
+}
+
+var btn = document.getElementById("my_buttion");
+btn.addEventListener('click', function click(evt){
+  console.log("buttion clicked");
+})
