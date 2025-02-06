07c407b4c95bc7ea4c759749d00bf97beddd5a4e
proavijit <avijitghosh2268@gmail.com>
2025-02-06 16:38:47 +0600

    coreation

---
+++ Type & Grammer/JSON.stringify.js
@@ -0,0 +1,61 @@
+
+
+
+// console.log(JSON.stringify(null));
+// console.log(JSON.stringify(function(){}));
+// console.log(JSON.stringify(1, undefined,function(){},4));
+// console.log(JSON.stringify({a: 1, b: 2},));
+
+
+
+
+
+
+
+
+// var a = {
+//     b: 42,
+//     c: 6
+// };
+
+
+// console.log(JSON.stringify(a));
+
+
+// var a = {
+//   val : [1, 2, 3],
+//   toJSON: function(){
+//     return this.val.slice(1);
+//   }
+// }
+
+// var b  = {
+//   val: [1, 2, 3],
+//   toJSON: function(){
+//     return '[' + this.val.slice(1).join() + ']';
+//   }
+// }
+
+// console.log(JSON.stringify(a));
+// console.log(JSON.stringify(b));
+
+// var a = {
+//     b: 42,
+//     c: "42",
+//     d: [1,2,3]
+// };
+
+// console.log(JSON.stringify( a, null, 3 );;
+
+
+
+// let obj = { name: "Avijit", age: 25, city: "Dhaka" };
+
+// console.log(JSON.stringify(obj, null, 4)); 
+
+
+// console.log("hello")
+
+// if (document.all) {
+//   console.log("This is Internet Explorer!");
+// }
---
+++ Type & Grammer/coartion.js
@@ -0,0 +1,77 @@
+// var myBirthday = new Date(1998, 10, 25); // ২৫ নভেম্বর, ১৯৯৮
+// console.log(myBirthday);
+
+
+
+
+// var timestamp1 = new Date().getTime();
+// console.log(timestamp1); // বর্তমান timestamp দেখাবে
+
+// var timestamp2 = Date.now(); 
+// console.log(timestamp2); // এটি একই কাজ করবে
+
+
+// if(!Date.now){
+//   Date.now = function(){
+//     return new Date().getTime();
+//   }
+// }
+
+// console.log(Date());
+
+
+
+// var error = new Error("Something went wrong");
+
+// console.log(error.message);
+
+
+
+// function checkAge(age){
+//   if(age < 18){
+//     throw new Error("You are not an adult");
+//   }
+//   return "You are an adult";
+// }
+
+// try{
+//   console.log(checkAge(15));
+// }catch(error){
+//   console.log(error.message);
+// }
+
+
+// var sym1 = Symbol("apple");
+// var sym2 = Symbol("apple");
+
+// console.log(sym1 === sym2);
+
+// console.log('------------------');
+
+// var obj = {
+//   name: "Avijit"
+// };
+
+
+// let text = "Developer";
+// console.log(text.slice(-3, 4));  // Output: ""
+// console.log(text.slice(3, -3));  // Output: "vel"
+
+
+// let text = "Programming";
+
+// console.log(text.substring(6,8)); 
+
+
+var obj = {
+  valueOf: function(){
+    return 42;
+  },
+  toString: function(){
+    return 4;
+  }
+}
+
+
+console.log(obj + "");
+console.log(obj * 2);
