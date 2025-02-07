a549da9f95fd991c83b1bd0b642b69dadf7f9724
proavijit <avijitghosh2268@gmail.com>
2025-02-07 12:01:45 +0600

    logical oparato r

--- app.js
+++ app.js
@@ -1,36 +1,6 @@
-// var num = 42;
-
-// // 1. String constructor
-// console.log(String(num)); // "42"
-
-// // 2. toString() method
-// console.log(num.toString()); // "42"
-
-// // 3. Template Literals
-// console.log(`${num}`); // "42"
 
 
-// let x = '44.44';
-
-// console.log(parseFloat(x)); // 44
-
-// var a = [1, 2];
-// var b = [3, 4];
-
-// console.log(a + b); 
-
-// function onlyOne(a, b, c){
-//   return !!(a && !b && !c) ||
-//           !!(b && !a && !c) ||
-//           !!(c && !a && !b);  
-// }
-
-// var a = true;
-// var b = false;
-//   console.log(onlyOne(a, b, b)); // true
-
-
-var a = true;
+var a = 'a';
 var b = "foo";
 
 console.log(a && b); // "42"
