f093c2a256108049ee25b2841eb2206d86cdd442
proavijit <avijitghosh2268@gmail.com>
2025-02-10 14:34:54 +0600

    promise explanation

--- Async_Performance/callback.js
+++ Async_Performance/callback.js
@@ -50,3 +50,99 @@ function requestData(url){
 requestData("http://example.com")
 .then(responce =>console.log(responce))
 .catch(error => console.log(error))
+var tracked = false;
+
+
+analytics.trackPurchase(purchaseData, function(){
+    if (!tracked) {
+        tracked = true;
+        chargeCreditCard();
+        displayThankyouPage();
+    }
+});
+
+
+function addNumbers(x, y) {
+  // নিশ্চিত করুন যে ইনপুটটি সংখ্যায় রয়েছে
+  if (typeof x != "number" || typeof y != "number") {
+    throw Error("Bad parameters");
+  }
+  // এখানে + নিরাপদে গাণিতিক যোগফল করবে
+  return x + y;
+}
+addNumbers(21, 21); // 42
+addNumbers(21, "21"); // Error: "Bad parameters"
+
+
+
+function sucess(data){
+  console.log(data);
+}
+
+function failure(err){
+  console.log(err);
+}
+
+ajax("http://some.url.1", success, failure)
+
+
+function timeoutPromise(delay){
+  return new Promise((resolve, reject) =>{
+    setTimeout(()=>{
+      reject("Timeout!")
+    },deley)
+  })
+}
+
+Promise.race([
+  foo(),
+  timeoutPromise(3000)
+])
+.then(function(){
+
+})
+
+function(err){
+
+}
+
+
+
+function makeCoffee(){
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      console.log("কফি তৈরি!");
+      resolve("কফি prostut")
+    })
+  })
+}
+
+// Step 2
+
+function doWork(coffe){
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      console.log('কিছু কাজ করা হচ্ছে...');
+      resolve("কাজ শেষ!")
+    }, 2000);
+  })
+}
+
+// Step 3 Netflix dakha
+
+function watchNetflix(work){
+  return new Promise((resolve, reject) =>{
+    setTimeout(() => {
+      console.log("Netflix দেখা হচ্ছে...");
+     resolve("Beeing watching seson is done...")
+    }, 1500);
+  })
+}
+
+
+
+makeCoffee()
+.then(coffee => doWork(coffee))
+.then(work => watchNetflix(work))
+.then(result => console.log(result);)
+.catch(err => console.log('একটা সমস্যা হয়েছে!', err);)
