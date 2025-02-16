eb7e4bb5014ca97ddb1230878a9cadb90ae5dad4
proavijit <avijitghosh2268@gmail.com>
2025-02-16 15:36:21 +0600

    Scope and all Behavear

--- app.js
+++ app.js
@@ -1,6 +1,110 @@
+// var Function Scope
+
+// var a = 2;
+// (()=> {
+//   var a = 3;
+//   console.log(a);
+// })();
+
+// console.log(a);
 
 
-import { formEvent } from 'rxjs';
 
 
-console.log(formEvent);
+
+
+
+
+
+// Let block Scope
+
+
+// var a = 2;
+
+// {
+//   let a = 3;
+//   console.log(a); // 3
+// }
+// console.log(a); //2
+
+
+
+
+
+
+// ES6 এর আগে var ব্যবহার করলে for লুপের ইনডেক্স ভেরিয়েবল পুরো ফাংশনে অ্যাক্সেস করা যেত। কিন্তু let ব্যবহার করলে এটি শুধুমাত্র ওই ব্লকের মধ্যেই থাকবে।
+
+// for (let i = 0; i < 4; i++) {
+//  console.log(i);
+  
+// }
+
+ // ReferenceError! কারণ i ব্লকের বাইরে নেই।
+
+
+
+
+
+
+
+
+
+
+// Temporal Dead Zone (TDZ
+// console.log(a); // ReferenceError!
+// let a = 5;
+// console.log(a); // এটি চলবে, প্রিন্ট করবে 5।
+
+
+// console.log(typeof a); // "undefined" (কারণ a ডিক্লেয়ার করা হয়নি)
+
+// var a = 5;
+
+
+// var funcs = [];
+
+// for (let i = 0; i < 6; i++) {
+ 
+//   funcs.push(function(){
+//     console.log(i);
+//   })
+  
+// }
+
+// funcs[3]()
+
+
+
+// let fruits = ["apple", "banana", "cherry"];
+
+// for(let fruit of fruits){
+//   setTimeout(() => {
+//     console.log(fruit);
+//   }, 1000);
+// }
+
+// "use strict"
+// {
+//   foo(); // ✅ কাজ করবে (Hoisting হয়েছে)
+  
+//   function foo() {
+//     console.log("Hello from foo!");
+//   }
+// }
+
+// foo(); // ❌ ReferenceError (ব্লকের বাইরে foo নেই)
+
+
+
+
+
+"use strict"
+
+
+if(true){
+  function test(){
+    console.log("Block Scope ata");
+  }
+}
+
+test()
